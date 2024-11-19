171dc16b78eafbc39ccabc1289134565bf3ba087
TujhZreitd <egor.yakushev.96@bk.ru>
2024-11-19 12:18:36 +0300

    commit init

---
+++ src/main/java/ru/javacode/paymentsmicroservice/handler/OrderCreatedEventHandler.java
@@ -0,0 +1,35 @@
+package ru.javacode.paymentsmicroservice.handler;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import ru.javacode.core.OrderCreatedEvent;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+@KafkaListener(topics = "new-orders-topic")
+@AllArgsConstructor
+public class OrderCreatedEventHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @KafkaHandler
+    public void handle(OrderCreatedEvent orderCreatedEvent) throws ExecutionException, InterruptedException {
+        LOGGER.info("Received event: {}", orderCreatedEvent.getNameProduct());
+        //TODO logic payment
+        SendResult<String, Object> result = kafkaTemplate
+                .send("payed-orders-topic", orderCreatedEvent.getProductId(), orderCreatedEvent).get();
+        LOGGER.info("Topic: {}", result.getRecordMetadata().topic());
+        LOGGER.info("Partition: {}", result.getRecordMetadata().partition());
+        LOGGER.info("Offset: {}", result.getRecordMetadata().offset());
+
+        LOGGER.info("Return: {}", orderCreatedEvent.getProductId());
+    }
+}
